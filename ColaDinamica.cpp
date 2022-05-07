9d5edf2a5ea32ac2280bf240d961ecc3dc8236de
SaidGama <sgamap8@gmail.com>
2022-05-07 00:32:38 -0500

    version final

--- ColaDinamica.cpp
+++ ColaDinamica.cpp
@@ -9,16 +9,15 @@ struct Node{
 Node *ptrCabeza = NULL;
 Node *ptrCola = NULL;
 
-//Function to check if queue is empty or not
+//Funcion para verificar si la cola esta vacio
 bool estaVacia(){
     if( ptrCabeza == NULL && ptrCola == NULL)
         return true;
     else
         return false;
 }
-//function to enter elements in queue
-void encolar ( int valor ){
-    
+
+void encolar (int valor){
     Node *nuevaCola = new Node();
     nuevaCola->dato= valor;
     nuevaCola->enlace=NULL;
@@ -32,24 +31,16 @@ void encolar ( int valor ){
         ptrCola = nuevaCola;
     }
 }
-
 void desencolar(){
     if ( estaVacia() ) //validamos el underflow
         cout<<"\nERROR el stack esta vacio";
-    else
-    if( ptrCabeza == ptrCola){
-        delete(ptrCabeza);
-        ptrCabeza = NULL;
-        ptrCola = NULL;
-    }else
-    {
+    else{
         Node *nodoTemporal = ptrCabeza; //en un nodo temporalle guardamos la direccion de la cabeza
         ptrCabeza = ptrCabeza -> enlace; //ahora la cima apuntara al siguiente nodo o conceptualmente al anterior
         delete(nodoTemporal); //liberamos memoria para eliminar el nodo 
     }
 }
-void displayStack()
-{
+void mostrarCola(){
    if( estaVacia() ){
        cout <<"cola vacia";
    }else{
@@ -64,16 +55,14 @@ void displayStack()
    }
 }
 int main(){
-
     int opcion, var=1, valor;
-
     while( var == 1){
         cout<<"\n1.encolar 2.Desencolar 3.mostrarCola 4.salir:  ";
         cin>>opcion;
         switch (opcion){
             case 1: cout<<"Introduzca el dato:\n";  cin>>valor; encolar(valor); break;
             case 2: desencolar(); break;         
-            case 3: displayStack(); break;
+            case 3: mostrarCola(); break;
             case 4: var = 0; break;
         }
     }
--- ColaPrioridad.cpp
+++ ColaPrioridad.cpp
@@ -10,53 +10,44 @@ struct Node{
 Node *ptrCabeza = NULL;
 Node *ptrCola = NULL;
 
-//Function to check if queue is empty or not
+//Funcion para verificar si la cola esta vacio
 bool estaVacia(){
     if( ptrCabeza == NULL && ptrCola == NULL)
         return true;
     else
         return false;
 }
-//function to enter elements in queue
-void encolar ( int valor, int p ){
+void encolar(int valor, int p){
     
     Node *nuevaCola = new Node(); //creo nueva cola
     nuevaCola->dato= valor; //le insertamos el valor a la cola
     nuevaCola->prioridad=p; //le insertmos la priorioridad
-    nuevaCola->enlace=NULL;
-    Node *ptrAnterior=NULL;
-    //este if es para cuando solo hay 1 elemento, en ese caso nuestro puntero cola y cabeza apuntaran al mismo nodo
-    if( ptrCabeza == NULL ){
+    //este if es para cuando no hay ningun nodo, en ese caso insertamos el nodo al principio
+    if( ptrCabeza == NULL || p <= ptrCabeza->prioridad){
+        nuevaCola->enlace=ptrCabeza;
         ptrCabeza = nuevaCola;
-        ptrCola = nuevaCola;
-    }
-    else{  //en caso contrario el puntero cola ahora si apuntara al nuevo nodo creado(conceptualmente nuestra nueva cola)
-        while( nuevaCola->prioridad < ptrCola->prioridad && ptrCabeza->enlace!=NULL){
-            ptrAnterior = ptrCola;
-            ptrCola = ptrCola->enlace;          
+    }else{
+        ptrCola = ptrCabeza;
+        while (ptrCola->enlace!=NULL && ptrCola->enlace->prioridad<=p)
+        {
+            ptrCola = ptrCola->enlace;
         }
-        nuevaCola->enlace=ptrAnterior->enlace;
-        ptrAnterior->enlace=nuevaCola;
+        nuevaCola->enlace = ptrCola->enlace;
+        ptrCola->enlace = nuevaCola;
     }
 }
 
 void desencolar(){
     if ( estaVacia() ) //validamos el underflow
         cout<<"\nERROR el stack esta vacio testing";
-    else
-    if( ptrCabeza == ptrCola){
-        delete(ptrCabeza);
-        ptrCabeza = NULL;
-        ptrCola = NULL;
-    }else
-    {
-        Node *nodoTemporal = ptrCabeza; //en un nodo temporalle guardamos la direccion de la cabeza
-        ptrCabeza = ptrCabeza -> enlace; //ahora la cima apuntara al siguiente nodo o conceptualmente al anterior
-        delete(nodoTemporal); //liberamos memoria para eliminar el nodo 
+    else{
+        Node *temporal = ptrCabeza;
+        ptrCabeza = ptrCabeza->enlace;
+        delete(temporal);
     }
+    
 }
-void displayStack()
-{
+void mostrarCola(){
    if( estaVacia() ){
        cout <<"cola vacia";
    }else{
@@ -71,11 +62,9 @@ void displayStack()
    }
 }
 int main(){
-
     int opcion, var=1, valor,prioridad;
-
     while( var == 1){
-        cout<<"\n1.encolar 2.Desencolar 3.mostrarCola 4.salir:  ";
+        cout<<"\n1.Encolar 2.Desencolar 3.mostrarCola 4.salir:  ";
         cin>>opcion;
         switch (opcion){
             case 1: 
@@ -86,7 +75,7 @@ int main(){
                 encolar(valor,prioridad); 
             break;
             case 2: desencolar(); break;         
-            case 3: displayStack(); break;
+            case 3: mostrarCola(); break;
             case 4: var = 0; break;
         }
     }
